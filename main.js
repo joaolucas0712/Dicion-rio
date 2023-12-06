e8b85f47fa2311fb60922a548e312b54c64087b1
João Lucas da Silva <133914805+joaolucas0712@users.noreply.github.com>
2023-12-05 22:43:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,248 @@
+let lista = document.querySelectorAll(".list")
+let elemento =  document.querySelectorAll("#elemento")
+let fechar = document.querySelectorAll("#fechar-modal")
+let audio = document.querySelectorAll("#audio")
+let botao = document.querySelectorAll("#botao")
+let conto = document.querySelector("#Conto")
+let conto2 = document.querySelector("#conto2")
+let citacoes = document.querySelectorAll("#citacoes")
+let img = document.querySelector("#img")
+let palavra = document.querySelector(".palavra")
+
+img.addEventListener("click", (e) =>{
+  palavra.style.display = "flex"
+})
+
+
+lista[0].addEventListener('click', (e) =>{
+  elemento[0].style.visibility = "visible"
+
+}
+)
+fechar[0].addEventListener("click", (e) =>{
+  elemento[0].style.visibility = "hidden"
+}
+)
+botao[0].addEventListener("mouseover", (e) => {
+  audio[0].play()
+})
+botao[0].addEventListener("mouseout", (e) => {
+  audio[0].pause()
+})
+citacoes[0].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+  
+})
+
+
+
+
+// 
+
+lista[1].addEventListener('click', (e) =>{
+  elemento[1].style.visibility = "visible"
+
+}
+)
+fechar[1].addEventListener("click", (e) =>{
+  elemento[1].style.visibility = "hidden"
+}
+)
+botao[1].addEventListener("mouseover", (e) => {
+  audio[1].play()
+})
+botao[1].addEventListener("mouseout", (e) => {
+  audio[1].pause()
+})
+citacoes[1].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+})
+citacoes[1].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+  
+})
+
+
+// 
+
+lista[2].addEventListener('click', (e) =>{
+  elemento[2].style.visibility = "visible"
+
+}
+)
+fechar[2].addEventListener("click", (e) =>{
+  elemento[2].style.visibility = "hidden"
+}
+)
+botao[2].addEventListener("mouseover", (e) => {
+  audio[2].play()
+})
+botao[2].addEventListener("mouseout", (e) => {
+  audio[2].pause()
+})
+citacoes[2].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+  
+})
+
+// 
+
+lista[3].addEventListener('click', (e) =>{
+  elemento[3].style.visibility = "visible"
+
+}
+)
+fechar[3].addEventListener("click", (e) =>{
+  elemento[3].style.visibility = "hidden"
+}
+)
+botao[3].addEventListener("mouseover", (e) => {
+  audio[3].play()
+})
+botao[3].addEventListener("mouseout", (e) => {
+  audio[3].pause()
+})
+citacoes[3].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+  
+})
+
+// 
+
+lista[4].addEventListener('click', (e) =>{
+  elemento[4].style.visibility = "visible"
+
+}
+)
+fechar[4].addEventListener("click", (e) =>{
+  elemento[4].style.visibility = "hidden"
+}
+)
+botao[4].addEventListener("mouseover", (e) => {
+  audio[4].play()
+})
+botao[4].addEventListener("mouseout", (e) => {
+  audio[4].pause()
+})
+citacoes[4].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+  
+})
+
+// 
+
+lista[5].addEventListener('click', (e) =>{
+  elemento[5].style.visibility = "visible"
+
+}
+)
+fechar[5].addEventListener("click", (e) =>{
+  elemento[5].style.visibility = "hidden"
+}
+)
+botao[5].addEventListener("mouseover", (e) => {
+  audio[5].play()
+})
+botao[5].addEventListener("mouseout", (e) => {
+  audio[5].pause()
+})
+citacoes[5].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+  
+})
+
+// 
+
+lista[6].addEventListener('click', (e) =>{
+  elemento[6].style.visibility = "visible"
+
+}
+)
+fechar[6].addEventListener("click", (e) =>{
+  elemento[6].style.visibility = "hidden"
+}
+)
+botao[6].addEventListener("mouseover", (e) => {
+  audio[6].play()
+})
+botao[6].addEventListener("mouseout", (e) => {
+  audio[6].pause()
+})
+citacoes[6].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+  
+})
+
+// 
+
+lista[7].addEventListener('click', (e) =>{
+  elemento[7].style.visibility = "visible"
+
+}
+)
+fechar[7].addEventListener("click", (e) =>{
+  elemento[7].style.visibility = "hidden"
+}
+)
+botao[7].addEventListener("mouseover", (e) => {
+  audio[7].play()
+})
+botao[7].addEventListener("mouseout", (e) => {
+  audio[7].pause()
+})
+citacoes[7].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+  
+})
+
+// 
+
+lista[8].addEventListener('click', (e) =>{
+  elemento[8].style.visibility = "visible"
+
+}
+)
+fechar[8].addEventListener("click", (e) =>{
+  elemento[8].style.visibility = "hidden"
+}
+)
+botao[8].addEventListener("mouseover", (e) => {
+  audio[8].play()
+})
+botao[8].addEventListener("mouseout", (e) => {
+  audio[8].pause()
+})
+citacoes[8].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+  
+})
+
+// 
+
+lista[9].addEventListener('click', (e) =>{
+  elemento[9].style.visibility = "visible"
+
+}
+)
+fechar[9].addEventListener("click", (e) =>{
+  elemento[9].style.visibility = "hidden"
+}
+)
+botao[9].addEventListener("mouseover", (e) => {
+  audio[9].play()
+})
+botao[9].addEventListener("mouseout", (e) => {
+  audio[9].pause()
+})
+citacoes[9].addEventListener("click", (e) => {
+  conto2.style.display = "flex"
+  
+})
+
+
+conto.addEventListener("click", (e) =>{
+  conto2.style.display = "flex"
+})
+fechar[10].addEventListener("click", (e) => {
+  conto2.style.display = "none"
+})
